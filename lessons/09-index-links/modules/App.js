5cd71d45d50ea7bbc2cdc271a067883dcb7280fd
wjfletcher <warrenjfletcher@gmail.com>
2017-02-28 18:05:37 -0500

    complete up to lesson 10

--- lessons/09-index-links/modules/App.js
+++ lessons/09-index-links/modules/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import NavLink from './NavLink'
+import { IndexLink } from 'react-router'
 
 export default React.createClass({
   render() {
@@ -9,6 +10,7 @@ export default React.createClass({
         <ul role="nav">
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/repos">Repos</NavLink></li>
+          <li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li>
         </ul>
         {this.props.children}
       </div>
